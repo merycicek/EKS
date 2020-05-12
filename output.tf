9d3d4ba4efeb1e87eb60cb878acbec7b5745b16b
Meryem Elibal <merycicek@gmail.com>
2020-05-12 21:07:06 +0000

    added out put

--- output.tf
+++ output.tf
@@ -1,3 +1,21 @@
 output "ARN" {
-  value = "module.my-cluster.cluster_arn"
+  value = module.my-cluster.cluster_arn
+}
+
+
+output "ID" {
+  value = module.my-cluster.cluster_id
+}
+
+
+
+output "Name" {
+  value = module.my-cluster.cluster_name
+}
+
+
+
+
+output "REGION" {
+  value = provider.aws.region
 }
