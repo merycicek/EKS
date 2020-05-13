e88b9a2568017865a76dc5dd08fc8081ffa7ab77
Meryem Elibal <merycicek@gmail.com>
2020-05-13 01:04:18 +0000

    ...

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform { 
 backend "s3" { 
 bucket = "meryemce-eks" 
-key = "application/us-east-1/state/dev/infrastructure.tfstate" 
+key = "application/us-east-1/state/dev/insfrastructure.tfstate.json" 
 region = "us-east-1" 
   } 
 } 
