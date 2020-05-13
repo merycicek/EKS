9b845d8e55130a726a3c1e5bd4bd6cd0d7981e3d
Meryem Elibal <merycicek@gmail.com>
2020-05-13 00:11:23 +0000

    ...

--- local.tf
+++ local.tf
@@ -11,7 +11,7 @@
 
 resource "null_resource" "login" {
     provisioner "local-exec" {
-    command = "aws eks --region var.region update-kubeconfig --name var.cluster_name"
+    command = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_name}"
     }
     provisioner "local-exec" {
     command = "kubectl get nodes"
